8de81f39063e5579861f3fbb151e35d3d936dbcf
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-06 11:50:16 +0300

    Add files via upload

---
+++ практикум 1/форма/задание 1 вариат 5/prac/prac/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace prac
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void a_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            r.Text = Convert.ToString(Math.Sqrt(Convert.ToDouble(a.Text) * Convert.ToDouble(a.Text) + Convert.ToDouble(b.Text) * Convert.ToDouble(b.Text)));
+        }
+
+        private void r_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
