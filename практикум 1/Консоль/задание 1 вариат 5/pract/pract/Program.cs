c53cf878db6382a2af5fcbe66d1f2749b46a0b8a
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-06 11:46:39 +0300

    Add files via upload

---
+++ практикум 1/Консоль/задание 1 вариат 5/pract/pract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, b, c;
+            Console.WriteLine("Введите катит а");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите катит b");
+            b = Convert.ToDouble(Console.ReadLine());
+            c = Math.Sqrt(a * a + b * b);
+            Console.WriteLine("Гипотенуза с = " + c);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ практикум 1/Консоль/задание 2 вариат 5/pract/pract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, D;
+            Console.WriteLine("Введите а");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите b");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите c");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a != 0)
+            {
+                D = b * b - 4 * a * c;
+                if (D > 0)
+                    Console.WriteLine("Решения есть");
+                else
+                    Console.WriteLine("Решения нет");
+            }
+            else
+                Console.WriteLine("Решения нет");
+        }
+    }
+}
