e842709c44dae021baf682139157b6ef088490db
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-07 12:26:50 +0300

    Add files via upload

---
+++ практикум 2/Консоль/задание 1 вариат 5/pract/pract/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x1, x2, x3, y1, y2, y3, S;
+            Console.WriteLine("Введите x1");
+            x1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y1");
+            y1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите x2");
+            x2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y2");
+            y2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите x3");
+            x3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y3");
+            y3 = float.Parse(Console.ReadLine());
+            S = ((x2 - x1)*(y3 - y1)) - ((x3 - x1)*(y2 - y1));
+            if (S < 0)
+                S *= -1;
+            Console.WriteLine("Площядь = " + S/2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ практикум 2/Консоль/задание 2 вариат 5/pract/pract/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            Console.WriteLine("Введите месяцы");
+            m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите дни");
+            n = Convert.ToInt32(Console.ReadLine());
+            switch (m%12)
+            {
+                case 0:
+                    Console.WriteLine("Январь");
+                    break;
+                case 1:
+                    Console.WriteLine("Февраль");
+                    break;
+                case 2:
+                    Console.WriteLine("Март");
+                    break;
+                case 3:
+                    Console.WriteLine("Апрель");
+                    break;
+                case 4:
+                    Console.WriteLine("Май");
+                    break;
+                case 5:
+                    Console.WriteLine("Июнь");
+                    break;
+                case 6:
+                    Console.WriteLine("Июль");
+                    break;
+                case 7:
+                    Console.WriteLine("Август");
+                    break;
+                case 8:
+                    Console.WriteLine("Сентябрь");
+                    break;
+                case 9:
+                    Console.WriteLine("Октябрь");
+                    break;
+                case 10:
+                    Console.WriteLine("Ноябрь");
+                    break;
+                case 11:
+                    Console.WriteLine("Декабрь");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
