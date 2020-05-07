466955956ea4df3f6887baa03477d73eb0e5fde2
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-07 12:40:14 +0300

    Add files via upload

---
+++ практикум 2/форма/задание 1 вариат 5/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int x, y;
+            x = Convert.ToInt32(A.Text);
+            y = Convert.ToInt32(B.Text);
+            if (x < 0)
+                x *= -1;
+            if (y < 0)
+                y *= -1;
+            if (x < 40)
+            {
+                if (y < 40)
+                    r.Text = "Да";
+                else if (y == 40)
+                    r.Text = "На гране";
+                else
+                    r.Text = "Нет";
+            }
+            else if (x == 40)
+            {
+                if (y == 40 || y < 40)
+                    r.Text = "На гране";
+                else
+                    r.Text = "Нет";
+            }
+            else
+                r.Text = "Нет";
+
+        }
+    }
+}
---
+++ практикум 2/форма/задание 2 вариат 5/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            switch (Convert.ToInt32(m.Text) % 12)
+            {
+                case 0:
+                    r.Text = "Январь";
+                    break;
+                case 1:
+                    r.Text = "Февраль";
+                    break;
+                case 2:
+                    r.Text = "Март";
+                    break;
+                case 3:
+                    r.Text = "Апрель";
+                    break;
+                case 4:
+                    r.Text = "Май";
+                    break;
+                case 5:
+                    r.Text = "Июнь";
+                    break;
+                case 6:
+                    r.Text = "Июлб";
+                    break;
+                case 7:
+                    r.Text = "Август";
+                    break;
+                case 8:
+                    r.Text = "Сентябрь";
+                    break;
+                case 9:
+                    r.Text = "Октябрь";
+                    break;
+                case 10:
+                    r.Text = "Ноябрь";
+                    break;
+                case 11:
+                    r.Text = "Декабрь";
+                    break;
+            }
+
+        }
+    }
+}
