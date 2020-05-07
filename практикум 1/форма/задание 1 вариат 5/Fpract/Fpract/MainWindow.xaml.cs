85f03bfb2d055b1e5e23bb7d385627052d545db5
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-07 09:59:21 +0300

    Add files via upload

---
+++ практикум 1/форма/задание 1 вариат 5/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            float sS;
+            sS = ((float.Parse(x2.Text) - float.Parse(x1.Text)) * (float.Parse(y3.Text) - float.Parse(y1.Text))) - ((float.Parse(x3.Text) - float.Parse(x1.Text)) * (float.Parse(y2.Text) - float.Parse(y1.Text)));
+            if (sS < 0)
+                sS *= -1;
+            S.Text = Convert.ToString(sS/2);
+        }
+    }
+}
