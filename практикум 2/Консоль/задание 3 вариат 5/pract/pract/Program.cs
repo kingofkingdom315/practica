36706e1e594fd81886316dd5285a18df526a1d8d
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-08 13:57:40 +0300

    Add files via upload

---
+++ практикум 2/Консоль/задание 3 вариат 5/pract/pract/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+            Console.WriteLine("Введие А");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введие B");
+            B = Convert.ToInt32(Console.ReadLine());
+            if (A < B)
+            {
+                Console.WriteLine("For");
+                int s, g = 0;
+                s = A;
+                for (int i = 0; i <= B - A; i++)
+                {
+                    if (s % 2 == 0 && s != 0 && s % 10 != 0)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    s++;
+                }
+                Console.WriteLine("While");
+                s = A;
+                while (s <= B)
+                {
+                    if (s % 2 == 0 && s != 0 && s % 10 != 0)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    s++;
+                }
+                Console.WriteLine("Do While");
+                s = A;
+                do
+                {
+                    if (s % 2 == 0 && s != 0 && s % 10 != 0)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    s++;
+                }
+                while (s <= B);
+            }
+            else
+            {
+                Console.WriteLine("A должно быть меньше B");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ практикум 2/Консоль/задание 4 вариат 5/pract/pract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+    }
+}
