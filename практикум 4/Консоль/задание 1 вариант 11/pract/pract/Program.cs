f23ac356f7fd71dff25cecfd531be0aa9da0b22e
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-15 13:25:06 +0300

    Add files via upload

---
+++ практикум 4/Консоль/задание 1 вариант 11/pract/pract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static int f(int n, int m1,int m2,int nx)
+        {
+            m1 = Math.Abs(m1)-6*m2;
+            if (nx != n)
+            {
+                nx++;
+                return f(n, m2, m1, nx);
+            }
+            else
+                return m1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 2)
+            {
+                Console.WriteLine("ответ: " + f(n, -10, 2, 3));
+            }
+            else
+                Console.WriteLine("Чичло n должно быть больше 2");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ практикум 4/Консоль/задание 2 вариант 11/pract/pract/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static void f(int n, int nx)
+        {
+            if (n % nx == 0 && n != nx)
+            {
+                Console.WriteLine(nx);
+                nx++;
+                f(n, nx);
+            }
+            else if (n != nx)
+            {
+                nx++;
+                f(n, nx);
+            }
+            else
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0)
+            {
+                f(n, 1);
+            }
+            else
+            {
+                Console.WriteLine("Число должно быть больше 0");
+            }
+            Console.ReadLine();
+        }
+    }
+}
