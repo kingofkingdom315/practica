63eeec44ee2782cfefaa19cdd24ee898a8770e95
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-15 13:32:08 +0300

    Add files via upload

---
+++ практикум 4/форма/задание 1 вариант 11/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        static int f(int n, int m1, int m2, int nx)
+        {
+            m1 = Math.Abs(m1) - 6 * m2;
+            if (nx != n)
+            {
+                nx++;
+                return f(n, m2, m1, nx);
+            }
+            else
+                return m1;
+        }
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToInt32(N.Text) > 2)
+            {
+                R.Text = Convert.ToString(f(Convert.ToInt32(N.Text), -10, 2, 3));
+            }
+            else
+                R.Text = "Чичло n должно быть больше 2";
+
+
+        }
+    }
+}
---
+++ практикум 4/форма/задание 2 вариант 11/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void f(int n, int nx, ref string t)
+        {
+            if (n % nx == 0 && n != nx)
+            {
+                t += Convert.ToString(nx) + "\n";
+                nx++;
+                f(n, nx, ref t);
+            }
+            else if (n != nx)
+            {
+                nx++;
+                f(n, nx, ref t);
+            }
+            else
+            {
+                t += Convert.ToString(n) + "\n";
+            }
+
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string s = "";
+            if (Convert.ToInt32(N.Text) > 0)
+            {
+                f(Convert.ToInt32(N.Text), 1,ref s);
+                R.Text = s;
+            }
+            else
+                R.Text = "Чичло n должно быть больше 0";
+
+
+        }
+    }
+}
