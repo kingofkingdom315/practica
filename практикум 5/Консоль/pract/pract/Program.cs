c4f068cd3ac3afa4b551c1657ccf9f062e8bcb90
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-15 14:36:28 +0300

    Add files via upload

---
+++ практикум 5/Консоль/pract/pract/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    {
+        static double f(double x)
+        {
+            try
+            {
+                if (x == 2) throw new Exception();
+                else if (x == 0) throw new Exception();
+                else return Math.Log(x/(x-2),Math.E);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введиет a = ");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите b = ");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введиет h = ");
+                double h = double.Parse(Console.ReadLine());
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        Console.WriteLine("y(" + i + ") = " + f(i));
+                    }
+                    catch
+                    {
+                        Console.WriteLine("y("+ i +") = ошибка");
+                    }
+                Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат ввода данных");
+            }
+            catch
+            {
+                Console.WriteLine("Неизвестная ошибка");
+            }
+        }
+    }
+}
+
---
+++ практикум 5/Форма/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        static double f(double x)
+        {
+            try
+            {
+                if (x == 2) throw new Exception();
+                else if (x == 0) throw new Exception();
+                else return Math.Log(x / (x - 2), Math.E);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                String s = "";
+                double a = double.Parse(A.Text);
+                double b = double.Parse(B.Text);
+                double h = double.Parse(H.Text);
+                for (double i = a; i <= b; i += h)
+                    try
+                    {
+                        s += "y(" + i + ") = " + f(i) + "\r";
+                    }
+                    catch
+                    {
+                        s += "y(" + i + ") = ошибка" + "\r";
+                    }
+                R1.Text = s;
+            }
+            catch (FormatException)
+            {
+                R1.Text = "Неверный формат ввода данных";
+            }
+            catch
+            {
+                R1.Text = "Неизвестная ошибка";
+            }
+
+
+        }
+    }
+}
