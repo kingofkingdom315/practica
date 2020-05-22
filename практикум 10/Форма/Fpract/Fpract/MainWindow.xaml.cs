acc2a0ab8bea5f4dc62115947f5a1daf92a0b321
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-22 17:01:03 +0300

    Add files via upload

---
+++ практикум 10/Форма/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string s = "";
+            if (!Directory.Exists("c:\\temp"))
+            {
+                Directory.CreateDirectory("c:\\temp");
+            }
+            Directory.CreateDirectory("c:\\temp\\K1");
+            Directory.CreateDirectory("c:\\temp\\K2");
+            StreamWriter sw = new StreamWriter("c:\\temp\\K1\\t1.txt");
+            sw.Write("Иванов Иван Иванович, 1965 года рождения, место жительства г. Саратов");
+            sw.Close();
+            sw = new StreamWriter("c:\\temp\\K1\\t2.txt");
+            sw.Write("Петров Сергей Федорович, 1966 года рождения, место жительства г.Энгельс");
+            sw.Close();
+            sw = new StreamWriter("c:\\temp\\K2\\t3.txt");
+            StreamReader sr = new StreamReader("c:\\temp\\K1\\t1.txt");
+            sw.WriteLine(sr.ReadToEnd());
+            sr.Close();
+            sr = new StreamReader("c:\\temp\\K1\\t2.txt");
+            sw.WriteLine(sr.ReadToEnd());
+            sr.Close();
+            sw.Close();
+            File.Move("c:\\temp\\K1\\t2.txt", "c:\\temp\\K2\\t2.txt");
+            File.Copy("c:\\temp\\K1\\t1.txt", "c:\\temp\\K2\\t1.txt");
+            Directory.Move("c:\\temp\\K2", "c:\\temp\\ALL");
+            Directory.Delete("c:\\temp\\K1", true);
+            DirectoryInfo dinf = new DirectoryInfo("c:\\temp\\ALL");
+            FileInfo[] finf = dinf.GetFiles();
+            foreach (FileInfo fi in finf)
+            {
+                s += fi.FullName.ToString() + fi.Attributes.ToString() + "\n";
+            }
+            A.Text = s;
+        }
+    }
+}
