01891ef58b90fc8e2e80622a2ce91a3d76820246
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-22 17:02:19 +0300

    Add files via upload

---
+++ практикум 7/Консоль/задание 1 вариант 11/pract/pract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pract
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            string s;
+            int a = 0;
+            Console.WriteLine("Введите s");
+            s = Console.ReadLine();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] >= 48 && s[i] <= 57)
+                {
+                    a += Convert.ToInt32(Convert.ToString(s[i]));
+                }
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
+
