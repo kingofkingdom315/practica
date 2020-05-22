6e70c7adc0a197bd5e27c956aad1c81373592c3b
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-22 17:05:14 +0300

    Add files via upload

---
+++ практикум 7/форма/задание 1 вариант 11/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string s = S.Text;
+            int a = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] >= 48 && s[i] <= 57)
+                {
+                    a += Convert.ToInt32(Convert.ToString(s[i]));
+                }
+            }
+            A.Text = Convert.ToString(a);
+            Console.WriteLine(a);
+
+        }
+    }
+}
---
+++ практикум 7/форма/задание 2 вариант 11/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string s = S.Text;
+            s = s.Replace(",", " ");
+            s = s.Replace(".", " ");
+            s = s.Replace("?", " ");
+            s = s.Replace("!", " ");
+            for (int i = 0; i < s.Length; i++)
+                s = s.Replace("  ", " ");
+            string[] s1 = s.Split(' ');
+            s = s1[0];
+            for (int i = 1; i < s1.Length; i++)
+                if (s.Length < s1[i].Length)
+                    s = s1[i];
+                else if (s.Length == s1[i].Length)
+                    s += ", " + s1[i];
+            A.Text = s;
+        }
+    }
+}
