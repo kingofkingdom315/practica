57c82aeebade8c9779781362dec967f32eaf1f72
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-22 17:09:50 +0300

    Add files via upload

---
+++ практикум 9/Консоль/Задание 1 Вариант 11/pract/pract/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace pract
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            double n = double.Parse(Console.ReadLine());
+            FileStream f = new FileStream("f.dat",FileMode.Open);
+            BinaryWriter fOut = new BinaryWriter(f);
+            double a;
+            for (Double i = 0; i < n; i++)
+            {
+                Console.WriteLine("Введите число");
+                a = double.Parse(Console.ReadLine());
+                fOut.Write(a);
+            }
+            fOut.Close();
+            f = new FileStream("f.dat", FileMode.Open);
+            BinaryReader fIn = new BinaryReader(f);
+            long m = f.Length;
+            for (long i = 0; i < m; i += 8)
+            {
+                f.Seek(i, SeekOrigin.Begin);
+                a = fIn.ReadDouble();
+                if (a < 0)
+                Console.Write("{0:f2} ", a);
+            }
+            fIn.Close();
+            f.Close();
+            Console.ReadLine(); 
+        }
+    }
+}
+
---
+++ практикум 9/Консоль/Задание 2 Вариант 11/pract/pract/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace pract
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            int n = int.Parse(Console.ReadLine());
+            string text = System.IO.File.ReadAllText("f.txt");
+            string[] s = text.Split("\n");
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].Length == n+1)
+                {
+                    Console.WriteLine(s[i]);
+                }
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
