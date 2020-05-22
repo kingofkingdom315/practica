73d7df6b1114729e2c8faf59b47c7c89e6bd5e7a
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-05-22 17:11:58 +0300

    Add files via upload

---
+++ практикум 9/Форма/Задание 1 Вариант 11/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string s = "";
+            double n = double.Parse(S.Text);
+            Random r = new Random();
+            FileStream f = new FileStream("f.dat", FileMode.Open);
+            BinaryWriter fOut = new BinaryWriter(f);
+            double a;
+            for (Double i = 0; i < n; i++)
+            {
+                a = r.Next(-10,10);
+                fOut.Write(a);
+                s += a + "\n";
+            }
+            C.Text = s;
+            s = "";
+            fOut.Close();
+            f = new FileStream("f.dat", FileMode.Open);
+            BinaryReader fIn = new BinaryReader(f);
+            long m = f.Length;
+            for (long i = 0; i < m; i += 8)
+            {
+                f.Seek(i, SeekOrigin.Begin);
+                a = fIn.ReadDouble();
+                if (a < 0)
+                    s += a + "\n";
+            }
+            A.Text = s;
+            fIn.Close();
+            f.Close();
+        }
+    }
+}
---
+++ практикум 9/Форма/Задание 2 Вариант 11/Fpract/Fpract/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace Fpract
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int n = int.Parse(S.Text);
+            string text = System.IO.File.ReadAllText("f.txt");
+            string[] s = text.Split('\n');
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].Length == n+1)
+                {
+                    text = s[i];
+                }
+            }
+            A.Text = text;
+        }
+    }
+}
