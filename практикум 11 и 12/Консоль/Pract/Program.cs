0b10398f7a9de1580376f560d92f5e645a2c1327
kingofkingdom315 <63289769+kingofkingdom315@users.noreply.github.com>
2020-10-20 19:38:51 +0300

    Add files via upload

---
+++ практикум 11 и 12/Консоль/Pract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(-5,12);
+            a.cordout();
+            Console.WriteLine(a.XO());
+            a.moving(5, -12);
+            a.cordout();
+            a.cordx = 6;
+            Console.WriteLine(a.cordx);
+            a.cordy = 6;
+            Console.WriteLine(a.cordy);
+            a.cordout();
+            a.Scal = 4;
+            a.cordout();
+            a++;
+            a.cordout();
+            a = a + 3;
+            a.cordout();
+            Console.ReadKey();
+        }
+    }
+}
